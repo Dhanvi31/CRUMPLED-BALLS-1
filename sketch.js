cba1e5fd9de9253a4026c1c256d01d42f673c19b
Dhanvi31 <79754778+Dhanvi31@users.noreply.github.com>
2021-04-07 06:14:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,36 +1,50 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
-}
-
+var ground, gameState,engine, world,dustbin,paper;
 function setup() {
-	createCanvas(800, 700);
+  createCanvas(800, 400);
+  rectMode(CENTER);
 
+  gameState = "start";
 
-	engine = Engine.create();
-	world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
 
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
-  
+  dustbin = new DustBin(720, 390, 100, 10);
+  paper = new Paper(100, 300, 10);
+  ground = Bodies.rectangle(width / 2, 400, width, 10,
+  {
+    isStatic: true
+  });
+  World.add(world, ground);
 }
-
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+  if (gameState === "start") {
+    background("black");
+    if (keyCode === UP_ARROW) {
+      gameState = "play"
+    }
+  }
+  if (gameState === "play") {
+    rectMode(CENTER);
+    background(0);
+    dustbin.display();
+    paper.display();
+
+  }
 }
 
 
-
+function keyPressed(){
+  if (keyCode === UP_ARROW && gameState === "play") {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 12,
+      y: -13
+    });
+  }
+}
